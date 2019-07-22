cf6d1b954a270cb10472d9072ddd05188c36ca73
Hadia Hameed <hadiahameed@MacBook-Pro.local>
2019-07-22 17:03:39 -0400

    Fixed the unit tests.

--- test/magicCoin.test.js
+++ test/magicCoin.test.js
@@ -3,29 +3,27 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const json = require('./../build/contracts/magicCoin.json');
 let accounts;
-let auction;
-let manager;
-const interface = json['abi'];
-const bytecode = json['bytecode'];
+let magicCoincontract;
+
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   receiver = accounts[1];
-  auction = await magicCoin.deployed();
+  magicCoincontract = await magicCoin.deployed();
 
 });
+
 describe('MagicCoin Smart contract', () => {
   it('Creating a coin:', async () => {
-    const auctionManager = await auction.owner.call();
-    await auction.create(auctionManager,100);
-    const senderBalance = await auction.balances.call(auctionManager);
+    const owner = await magicCoincontract.owner.call();
+    await magicCoincontract.create(owner,100);
+    const senderBalance = await magicCoincontract.balances.call(owner);
     assert.equal(100, senderBalance.toNumber(), "Coin Created.")
   });
 
   it('Transfering coin:', async () => {
-    await auction.transfer(receiver,5);
-    const receiverBalance = await auction.balances.call(receiver);
+    await magicCoincontract.transfer(receiver,5);
+    const receiverBalance = await magicCoincontract.balances.call(receiver);
     assert.equal(5, receiverBalance.toNumber(), "Coin Transferred.")
   });
 });
