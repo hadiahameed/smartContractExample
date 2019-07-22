cf1607dc990cc97d6a3a79a2c8ec9293d4d2cb49
Hadia Hameed <hadiahameed@MacBook-Pro.local>
2019-07-22 17:13:30 -0400

    edited contract

--- contracts/magicCoin.sol
+++ contracts/magicCoin.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.25;
 
 contract magicCoin {
-    address owner;
+    address public owner;
     mapping (address => uint) public balances;
     event Delivered(address from, address to, uint amount);
 
@@ -9,15 +9,20 @@ contract magicCoin {
         owner = msg.sender;
     }
 
+    //adds balance to the owner's account
     function create (address receiver, uint amount) public {
         require(msg.sender == owner, 'Unauthorized creator.');
+
         balances[receiver] += amount;
     }
 
+    //transfers coins from owner to receiver
     function transfer (address receiver, uint amount) public {
-        require(balances[receiver] >= amount, "You don't have enough balance." );
+        require(balances[msg.sender] >= amount, "You don't have enough balance." );
+
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
+
         emit Delivered(msg.sender, receiver, amount);
     }
 }
